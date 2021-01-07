b1fac687edbf8c5514ac52eb1edc98b79c512095
gary909 <garywhite61@hotmail.com>
2021-01-07 15:50:04 +0100

    updates to code

--- manager.js
+++ manager.js
@@ -1,10 +1,15 @@
 window.onload = function() {
-    var worker = new Worker("worker.js");
-
-    worker.postMessage("ping");
-
-    worker.onmessage = function (event) {
-        var message = "Worker says " + event.data;
-        document.getElementById("output").innerHTML = message;
-    }
+	var numWorkers = 3;
+	var workers = [];
+	for (var i = 0; i < numWorkers; i++) {
+		var worker = new Worker("worker.js");
+		worker.onmessage = function(event) {
+			alert(event.target + " says " + event.data);
+		};
+		workers.push(worker);
+	}
+	for (var i = 0; i < workers.length; i++) {
+		workers[i].postMessage("ping");
+	}
 }
+
